a2befbdf187323e3d8a8e2504fdbccf9548ec2a3
zhongshuiping <zhongshuiping@deppon.com>
2022-08-01 10:19:38 +0800

    提交 Android框架项目  提交本地maven私服搭建

--- app/src/main/java/com/example/androidframework/MainActivity.java
+++ app/src/main/java/com/example/androidframework/MainActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        Executors.newFixedThreadPool();
     }
 }
